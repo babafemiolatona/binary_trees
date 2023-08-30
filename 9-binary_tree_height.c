342e3d4c41030face3303b504ce0f38951f68c17
babafemiolatona <femiolatona@gmail.com>
2023-08-30 18:01:46 +0100

    Refactored code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,17 +8,20 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_st_height, right_st_height;
+	size_t l_height, r_height;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 	{
 		return (0);
 	}
+	l_height = binary_tree_height(tree->left) + 1;
+	r_height = binary_tree_height(tree->right) + 1;
+	if (l_height > r_height)
+	{
+		return (l_height);
+	}
 	else
 	{
-		left_st_height = binary_tree_height(tree->left);
-		right_st_height = binary_tree_height(tree->right);
-		return (1 + (left_st_height > right_st_height ?
-		left_st_height : right_st_height));
+		return (r_height);
 	}
 }
